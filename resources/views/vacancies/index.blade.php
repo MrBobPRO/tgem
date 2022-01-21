22b6e6541836762c365623c2bbc0cb9876cf0a5f
MrBobPro <boburjon_n@mail.ru>
2022-01-21 13:18:37 +0500

    Vacancies edited

--- resources/views/vacancies/index.blade.php
+++ resources/views/vacancies/index.blade.php
@@ -9,13 +9,9 @@
     <div class="main-container vacancies-page__inner">
         <div class="vacancies-list">
             @foreach ($vacancies as $vacancy)
-                <div class="vacancies-list__item">
+                <a class="vacancies-list__item" href="{{ route('vacancies.single', $vacancy->url) }}">
                     <img class="vacancies-list__image" src="{{ asset('img/archive/medium/' . $vacancy->image) }}" alt="{{ $vacancy->title }}">
-                    <a class="vacancies-list__link" href="{{ route('vacancies.single', $vacancy->url) }}">
-                        <span class="vacancy-list__span">Вакансия</span>
-                        <h2 class="vacancy-list__title">{{ $vacancy->title }}</h2>
-                    </a>
-                </div>
+                </a>
             @endforeach
         </div>
     </div>
