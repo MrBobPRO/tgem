ca2ac9a563300f25d4f76960e65afd29c05aa52b
MrBobPRO <bobnuridinov@gmail.com>
2022-06-30 14:41:38 +0500

    Analitycs fixed

--- resources/views/templates/analytics.blade.php
+++ resources/views/templates/analytics.blade.php
@@ -1,15 +1,6 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123986695-56"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-123986695-56');
-</script>
-
-
+<!-- Yandex.Metrika counter -->
 <script type="text/javascript" >
-    (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
     m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
  
@@ -21,3 +12,14 @@
     });
  </script>
  <noscript><div><img src="https://mc.yandex.ru/watch/49300630" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+ <!-- /Yandex.Metrika counter -->
+ 
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+ <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123986695-56"></script>
+ <script>
+   window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
+ 
+   gtag('config', 'UA-123986695-56');
+ </script>
